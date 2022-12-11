0762ad9db06e0f4b95390e4115811938ee31276d
jennifxz <jenniferoliveira362@gmail.com>
2022-12-10 23:14:31 -0300

    changes...............

--- src/App.js
+++ src/App.js
@@ -5,7 +5,21 @@ import CardEspiralUsuarioDark from './components/cardDark';
 function App() {
   return (
     <div className="pagina-dos-cards">
-      <CardEspiralUsuarioDark espiral='01' classe='nAvaliado' infos='Aborda conceitos sobre variáveis, tipagem, inferência, conversão e outras coisas.'/>
+      <div className='cla'>
+      <CardEspiralUsuarioDark espiral='01' classe='aprovadoMaisUm' infos='Aborda conceitos sobre variáveis, tipagem, inferência, conversão e outras coisas.'/>
+      <CardEspiralUsuarioDark espiral='02' classe='ajustes' infos='Aborda conceitos sobre variáveis, tipagem, inferência, conversão e outras coisas.'/>
+      <CardEspiralUsuarioDark espiral='03' classe='bloqueada' infos='Aborda conceitos sobre variáveis, tipagem, inferência, conversão e outras coisas.'/>
+      </div>
+      <div className='cla'>
+      <CardEspiralUsuarioDark espiral='04' classe='aprovadoMaisUm' infos='Aborda conceitos sobre variáveis, tipagem, inferência, conversão e outras coisas.'/>
+      <CardEspiralUsuarioDark espiral='05' classe='ajustes' infos='Aborda conceitos sobre variáveis, tipagem, inferência, conversão e outras coisas.'/>
+      <CardEspiralUsuarioDark espiral='06' classe='bloqueada' infos='Aborda conceitos sobre variáveis, tipagem, inferência, conversão e outras coisas.'/>
+      </div>
+      <div className='cla'>
+      <CardEspiralUsuarioDark espiral='07' classe='aprovadoMaisUm' infos='Aborda conceitos sobre variáveis, tipagem, inferência, conversão e outras coisas.'/>
+      <CardEspiralUsuarioDark espiral='08' classe='ajustes' infos='Aborda conceitos sobre variáveis, tipagem, inferência, conversão e outras coisas.'/>
+      <CardEspiralUsuarioDark espiral='09' classe='bloqueada' infos='Aborda conceitos sobre variáveis, tipagem, inferência, conversão e outras coisas.'/>
+      </div>
     </div>
   );
 }
--- src/components/cardDark/index.js
+++ src/components/cardDark/index.js
@@ -2,21 +2,27 @@ import './index.scss'
 
 export default function CardEspiralUsuarioDark(props) {
   return (
-    <main className='card-espiral-usuario-dark' >
-      <div className='espiral-icones'>
-        <h2>Espiral {props.espiral}</h2>
+    <main className='card-espiral-usuario-dark-main' >
+
+      <div className='espiral-principal'>
         <div className={props.classe}>
-          <img className='fire' src='/assets/images/fire.svg' alt='' />
-          <img className='tool' src='/assets/images/tool.svg'  alt='' />
-          <img className='check' src='/assets/images/check.svg' alt='' />
-          <img className='lock' src='/assets/images/lock.svg'   alt='' />
+          <div className='espiral-icones'>
+          <h2>Espiral {props.espiral}</h2>
+            <img className='fire' src='/assets/images/fire.svg' alt='' />
+            <img className='tool' src='/assets/images/tool.svg' alt='' />
+            <img className='check' src='/assets/images/check.svg' alt='' />
+            <img className='lock' src='/assets/images/lock.svg' alt='' />
+          </div>
+          <div className='espiral-infos'>
+            <p>{props.infos}</p>
+          </div>
+         <div className='espiral-acesso'>
+            <button>Acessar</button>
+         </div>
+
+
+
         </div>
-      </div>
-      <div className='espiral-infos'>
-        <p>{props.infos}</p>
-      </div>
-      <div className='espiral-acesso'>
-        <p>Acessar</p>
       </div>
 
     </main>
